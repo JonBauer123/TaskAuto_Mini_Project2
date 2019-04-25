5e9e89d53dbf5bd9be93467cb5f11558b7279bcf
JonBauer123 <wcjon123@gmail.com>
2019-04-25 14:55:10 -0400

    BLAH

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,3 +1,11 @@
 def compute(nodes):
 	print('called compute function in compute_metrics.py')
 
+	for thing in nodes:
+		for i in range(0,len(thing.packets)):
+			if thing.packets[i].type == "request":
+				print "REQUEST: " + thing.packets[i].src
+			if thing.packets[i].type == "reply":
+				print "REPLEY: " + thing.packets[i].dst
+
+		print "-----------------"
